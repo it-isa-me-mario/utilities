434901857d4f6d9939d82ff9b3483f804819d0d0
Enes Genç <enesgenc4661@gmail.com>
2021-12-04 22:26:53 +0300

    fix: update rules

--- packages/eslint-config-vue/src/eslintrc.ts
+++ packages/eslint-config-vue/src/eslintrc.ts
@@ -3,17 +3,19 @@ export const eslintConfig = {
     "@akualabs",
     "plugin:vue/vue3-strongly-recommended",
   ],
-  "vue/multi-word-component-names": 0,
-  "vue/html-closing-bracket-spacing": ["error", {
-    "startTag": "never",
-    "endTag": "never",
-    "selfClosingTag": "never",
-  }],
-  "vue/html-quotes": [2, "double", { "avoidEscape": true }],
-  "vue/max-attributes-per-line": 0,
-  "vue/component-tags-order": ["error", {
-    "order": ["script", "template", "style"],
-  }],
+  rules: {
+    "vue/multi-word-component-names": 0,
+    "vue/html-closing-bracket-spacing": ["error", {
+      "startTag": "never",
+      "endTag": "never",
+      "selfClosingTag": "never",
+    }],
+    "vue/html-quotes": [2, "double", { "avoidEscape": true }],
+    "vue/max-attributes-per-line": 0,
+    "vue/component-tags-order": ["error", {
+      "order": ["script", "template", "style"],
+    }],
+  },
 };
 
 module.exports = eslintConfig;
